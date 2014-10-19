ee37d280c970344674660c06010373e7aee865b2
Joe Gutierrez <joe@brightroll.com>
2014-10-18 18:08:07 -0700

    wip : default table view thanks to http://adoptioncurve.net/archives/2014/07/a-minimum-viable-tableview-in-swift/

--- FightTracker/ResultsListController.swift
+++ FightTracker/ResultsListController.swift
@@ -8,16 +8,51 @@
 
 import UIKit
 
-class ResultsListController: UIViewController {
+class ResultsListController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    let cellIdentifier = "cellIdentifier"
+    var tableData = ["joe", "jeng", "carleton", "chewy", "carl"]
+
+    @IBOutlet var tableView: UITableView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // Register the UITableViewCell class with the tableView
+        self.tableView?.registerClass(UITableViewCell.self, forCellReuseIdentifier: self.cellIdentifier)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
+    // UITableViewDataSource methods
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableData.count
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier(self.cellIdentifier) as UITableViewCell
+
+        cell.textLabel?.text = self.tableData[indexPath.row]
+
+        return cell
+    }
+
+    // UITableViewDelegate methods
+    func tableView(tableView: UITableView!, didSelectRowAtIndexPath indexPath: NSIndexPath!) {
+        let alert = UIAlertController(title: "Item selected", message: "You selected item \(indexPath.row)", preferredStyle: UIAlertControllerStyle.Alert)
+
+        alert.addAction(UIAlertAction(title: "OK",
+                                      style: UIAlertActionStyle.Default,
+                                    handler: {
+                                        (alert: UIAlertAction!) in println("An alert of type \(alert.style.hashValue) was tapped!")
+                                    }))
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+
 }
